6c74d47f4120702200ba9f4b30ea599c925d6db0
sofia <sbrown@fwparker.org>
2020-02-11 14:22:54 -0600

    added imports notes

--- Notes/NotesA/04_searching.py
+++ Notes/NotesA/04_searching.py
@@ -1,7 +1,6 @@
 # Searching
 
 # use forward slashes to go into folders and .. to go "up" a folder
-# can do control click then copy path, not always what we want though
 file = open('../resources/super_villains.txt', 'r')  # open to read
 print(file)
 
@@ -10,37 +9,39 @@ for line in file:
 
 file.close()
 
-# .strip() method removes extra characters on either side of text
+# .strip() method removes the extra characters at end of text
 print("    Hello ".strip())
-print("World\n".strip())  # took away double space
+print("World\n".strip())
 print("!")
 
-
-# 'w' open to write & overrides file
-file = open('../resources/super_villains.txt', 'a')  # 'a' opens to append to file
+# 'w' opens to write and overwrites the file
+# 'a' opens to append
+file = open('../resources/super_villains.txt', 'a')
 file.write('Lee the Merciless\n')
 file.write('Rohan the Destroyer\n')
 file.close()
 
+# open to write can create a new file
 file = open('../resources/heroes.txt', 'w')
 file.write('Owen the Valiant\n')
 file.close()
 
-# Better way to open close a file (syntactic sugar)
+#  Better way to open close a file (syntactic sugar)
 # file automatically closes after execution of with
 with open('../resources/super_villains.txt') as f:
     for line in f:
         print(line.strip())
 
+
 # .read() method just imports whole file as a string
 with open('../resources/super_villains.txt') as f:
     read_data = f.read()  # big string
 
-print("\n\nRead method:")
+print("\n\nRead method")
 print(read_data)
 
 # Reading data into an array (list)
-with open('../resources/super_villains.txt') as f:
+with open('../resources/super_villains.txt', 'r') as f:
     villains = [x.strip().upper() for x in f]
 
 print(villains)
@@ -50,25 +51,70 @@ print(villains)
 key = "Vidar the Manic".upper()
 
 i = 0
-while i < (len(villains) - 1) and key != villains[i]:  # stay in list, if the key not equal to i then keep going thru loop
+while i < (len(villains)) and key != villains[i]:
     i += 1
 
 if i < len(villains):
     print("Found it at position", i)
 else:
-    print(key, "is not in list")
+    print(key, "is not in the list")
 
 # try to make this into a function
 
-
-def linear_search(key, list):
+def linear_search(key, my_list):
     """
     :param key: what you are looking for
     :param list: where you are looking
     :return: bool did you find it?
     """
+    i = 0
+    while i < (len(my_list) - 1) and key.upper() != my_list[i]:
+        i += 1
+
+    if i < len(villains) - 1:
+        print("Found", key, "at position", i)
+        return True
+    else:
+        print(key, "not found.")
+        return False
+
+# Binary Search
+
+villains.sort()
+
+key = "THEODORA THE WICKED"
+lower_bound = 0
+upper_bound = len(villains) - 1
+found = False
+
+while lower_bound <= upper_bound and not found:
+    middle_pos = (upper_bound + lower_bound) // 2  # average
+    if villains[middle_pos] < key:
+        lower_bound = middle_pos + 1
+    elif villains[middle_pos] > key:
+        upper_bound = middle_pos - 1
+    else:
+        found = True
+
+if found:
+    print(key, "was found at position", middle_pos)
+else:
+    print(key, "was not found")
+
+'''
+# GIFTED FUNCTION
+# returns a list of words in each line
+
+import re  # regular expression
+
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+my_text = "Hello, this is Aaron's phone!"
+print(split_line(my_text))
+'''
 
 
-print(linear_search())
+
 
 
