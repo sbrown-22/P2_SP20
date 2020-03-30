cd45feb2406bc7cdd430943d36567b00f3068b33
sofia <sbrown@fwparker.org>
2020-03-30 09:21:14 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Labs/CTA Ridership/cta_rider_lab.py
    	Notes/NotesA/10_plotting_csv.py

--- Notes/NotesA/10_plotting_scatter.py
+++ Notes/NotesA/10_plotting_scatter.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open("World firearms murders and ownership - Sheet 1.tsv") as f:
     reader = csv.reader(f, delimiter="\t")
@@ -17,7 +18,7 @@ similar = ["Canada", "Norway", "Australia", "Iceland", "Finland", "Denmark", "Sw
            "Germany", "Japan", "Taiwan", "Singapore", "Netherlands", "Italy", "Spain",
            "England and Wales", "United States", "South Korea"]  # if add "El Salvador", will change A LOT
 
-# data = [x for x in data if x[0] in similar]
+data = [x for x in data if x[0] in similar]  # comment this out to get original graphs with all countries
 
 for country in data:
     try:
@@ -43,5 +44,14 @@ plt.title("Homicides vs. Gun Ownership")
 
 for i in range(len(names)):
     plt.annotate(names[i], xy=(firearm_100[i], homicide_100k[i]))
+
+p = np.polyfit(firearm_100, homicide_100k, 1)  # (x, y, poly_order) 1st order is linear
+print(p)  # returns (m, b) (slope, y intercept)
+
+# line of best fit - can kind of predict
+x = [x for x in range(100)]  # 0 to 99, make data to plot
+y = [p[0] * y + p[1] for y in x]  # this is the equation of the line, for every y in the x
+
+plt.plot(x, y)
 
 plt.show()
