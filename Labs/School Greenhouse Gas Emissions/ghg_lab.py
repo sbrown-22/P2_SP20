42e77bf6db6b6c65a1a03d253000433e7120e310
sofia <sbrown@fwparker.org>
2020-04-03 13:37:09 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Labs/CTA Ridership/cta_rider_lab.py
    	Notes/NotesA/10_plotting_csv.py

--- Labs/School Greenhouse Gas Emissions/ghg_lab.py
+++ Labs/School Greenhouse Gas Emissions/ghg_lab.py
@@ -1,4 +1,4 @@
-'''
+"""
 Greenhouse gas emissions (GHG) vs. square footage for all school buildings in Chicago
 
 Data set used will be Chicago Energy Benchmark info from 2018
@@ -8,7 +8,7 @@ https://data.cityofchicago.org/api/views/xq83-jr8c/rows.csv?accessType=DOWNLOAD
 Energy Efficiency of Chicago Schools (35pts)
 
 Chicago requires that all buildings over 50000 square feet in the city comply with energy benchmark reporting each year.
-The dataset at the link above is that data from 2015 to 2018.
+The data set at the link above is that data from 2015 to 2018.
 We will use this data to look at schools.  We will visualize the efficiency of schools by scatter plot.
 We expect that the more square footage (sqft) a school is, the more greenhouse gas (ghg) emission it will produce.
 Challenge (for fun):
@@ -16,7 +16,7 @@ An efficient school would have a large ratio of sqft to ghg.
 It would also be interesting to know where Parker lies on this graph???  Let's find out.
 
 Make a scatterplot which does the following:
-- Scatter plot the Total Greenhouse gas (GHG) Emmissions (y-axis), versus building square footage (x-axis) (10pts)
+- Scatter plot the Total Greenhouse gas (GHG) Emissions (y-axis), versus building square footage (x-axis) (10pts)
 - Data includes ONLY data for K-12 Schools. (5pts)
 - Data includes ONLY data for 2018 reporting. (5pts)
 - Label x and y axis and give appropriate title. (5pts)
@@ -30,19 +30,19 @@ Note: With extra credit you will earn you a max of 35pts (100%) for the assignme
 Maybe you can try one of the following or think up your own:
 - Annotated labels (school name) for the 3 highest and 3 lowest GHG Intensities.
 - Make schools in top 10 percent of GHG Intensity show in green.
-- Make schools in bottom 10 percent GHG Intesity show in red.
+- Make schools in bottom 10 percent GHG Intensity show in red.
 - Add colleges and universities (use a different marker type)
 
 
 Note 2:  This is a tough assignment to do on your own.  Do your best with what you have.
-'''
+"""
 
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
 with open("Chicago_Energy_Benchmarking (2).csv") as f:
-    reader = csv.reader(f)  # , delimiter="\t"
+    reader = csv.reader(f)
     data = list(reader)
 
 header = data.pop(0)
@@ -51,9 +51,11 @@ print(header)
 ghg_index = header.index("Total GHG Emissions (Metric Tons CO2e)")
 sqft_index = header.index("Gross Floor Area - Buildings (sq ft)")
 type_index = header.index("Primary Property Type")
+# intensity_index = header.index("GHG Intensity (kg CO2e/sq ft)")
 
 valid_data = []
 print(len(data))
+
 
 for building in data:
     try:
@@ -61,21 +63,56 @@ for building in data:
         float(building[sqft_index])
         if building[type_index] == "K-12 School" and building[0] == "2018":
             valid_data.append(building)
+
     except:
         pass
 
-print(len(valid_data))
+valid_data.sort(key=lambda x: float(x[ghg_index + 1]))
 
 ghg = [float(x[ghg_index]) for x in valid_data]
 sqft = [float(x[sqft_index]) for x in valid_data]
 
-plt.figure(1, tight_layout=True, figsize=(14, 7))
+ghg_intensity = [float(x[25]) for x in valid_data]
 
-plt.scatter(sqft, ghg, alpha=0.3)  # s for size, c for color (arrays)
+top_ten_intensity = ghg_intensity[-10:]
+bottom_ten_intensity = ghg_intensity[:10]
+
+print(bottom_ten_intensity)
+
+colors = ["gray" for x in valid_data]
+for i in range(len(top_ten_intensity)):
+    colors[i] = "green"
+for i in range(len(bottom_ten_intensity)):  # fix all this
+    colors[-i - 1] = "red"
+
+
+plt.figure(1, figsize=(14, 7))
+
+plt.scatter(sqft, ghg, alpha=0.3, c=colors)
 
 plt.ylabel("GHG Emissions")
 plt.xlabel("Square Footage")
 plt.title("GHG Emissions vs. Square Footage (K-12 schools in 2018)")
+
+
+name_index = header.index("Property Name")
+parker_index = [x[name_index] for x in valid_data].index("Francis W Parker School")
+
+
+# annotate Parker's name
+for data in valid_data:
+    if data[name_index] == "Francis W Parker School":
+        plt.annotate(data[name_index], xy=(sqft[parker_index], ghg[parker_index]))
+        plt.scatter(sqft[parker_index], ghg[parker_index])
+
+# line of best fit
+p = np.polyfit(sqft, ghg, 1)
+print(p)
+
+x = [x for x in range(700000)]
+y = [p[0] * y + p[1] for y in x]
+
+plt.plot(x, y)
 
 
 plt.show()
--- Notes/NotesA/10_plotting_url.py
+++ Notes/NotesA/10_plotting_url.py
@@ -1,25 +1,20 @@
-"""
+'''
 Reading a csv from a url
-Comma Separated Values (CSV) from a Uniform Resource Locator (URL)
-
-scatter plot w/ sizes and colors
-"""
+(Comma Separated Values from Uniform Resource Locator)
+scatter plot with colors and sizes
+'''
 import csv
 import requests
 import matplotlib.pyplot as plt
 
-
 def get_data(url):
-    with requests.Session() as s:  # making a class object out of Request library
-        download = s.get(url)  # download data
-        content = download.content.decode('utf-8')  # way to format text
-        reader = csv.reader(download.splitlines(),
-                            delimiter=',')  # or content.splitlines(), the comma makes it plit data iby commas
+    with requests.Session() as s:
+        download = s.get(url)
+        download = download.content.decode('utf-8')
+        reader = csv.reader(download.splitlines(), delimiter=",")
         data = list(reader)
     return data
 
-
-# so that we can get updated data each time
 
 url = "https://data.cityofchicago.org/api/views/xq83-jr8c/rows.csv?accessType=DOWNLOAD"
 data = get_data(url)
@@ -31,10 +26,12 @@ ghg_index = header.index("Total GHG Emissions (Metric Tons CO2e)")
 sqft_index = header.index("Gross Floor Area - Buildings (sq ft)")
 type_index = header.index("Primary Property Type")
 
-valid_data = []
+print(data[:10])
 print(len(data))
 
-for building in data:  # rebuilding list
+valid_data = []
+
+for building in data:
     try:
         float(building[ghg_index])
         float(building[sqft_index])
@@ -50,24 +47,13 @@ valid_data.sort(key=lambda x: float(x[ghg_index + 1]))
 ghg = [float(x[ghg_index]) for x in valid_data]
 sqft = [float(x[sqft_index]) for x in valid_data]
 
-
 colors = ["green" for x in valid_data]
 for i in range(37):
     colors[i] = "red"
 
-'''
-color = []
-
-for building in valid_data:
-    if building > 4000:
-        color.append("red")
-    else:
-        color.append("green")
-'''
 
 plt.figure("GHG School Plot")
-# plt.figure(1, tight_layout=True)
 
-plt.scatter(ghg, sqft, alpha=0.3, c=colors)  # s for size, c for color (arrays
+plt.scatter(ghg, sqft, alpha=0.3, c=colors)  # s for size and c for color (arrays)
 
 plt.show()
