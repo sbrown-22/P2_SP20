b7aae026ca0e0680104e91dca42252e52d29dc04
sofia <sbrown@fwparker.org>
2020-04-20 10:03:43 -0500

    Merge branch 'master' of https://github.com/fwparkercode/P2_SP20
     Conflicts:
    	Labs/CTA Ridership/cta_rider_lab.py
    	Notes/NotesA/10_plotting_csv.py

--- Labs/Scraping Lab/scraping-lab.py
+++ Labs/Scraping Lab/scraping-lab.py
@@ -1,13 +1,42 @@
 # SCRAPING PROBLEMS
+
 # Twitter Scraping (15pts)
 # Go to your favorite follow on Twitter.  (not someone who posts explicit materials please)
 # Inspect the twitter feed in Chrome.
 # You'll notice that the tweets are stored in a ordered list <ol></ol>, and individual tweets are contained as list items <li></li>.
-# Use BeautifulSoup and requests to grab the text contents of last 5 tweetslocated on the twitter page you chose.
+# Use BeautifulSoup and requests to grab the text contents of last 5 tweets located on the twitter page you chose.
 # Print the tweets in a nicely formatted way.
 # Have fun.  Again, nothing explicit.
 
-#  print("{} {}!".format("Hello", "World"))
+# There was some difficulty scraping a tweet.
+# After a closer look, you can't do it from the dynamic webpage any longer
+# I will give you some separate code to show you how to scrape a dynamic page.
+# Feel free to skip if you don't want to try it.  (no penalty)
+
+from bs4 import BeautifulSoup
+import requests
+
+'''
+from selenium import webdriver
+import time
+
+# I inspected a tweet and saw this span tag...
+# <span class="class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0">FINE LINE. THE ALBUM. OUT NOW. </span>
+
+harry_url = "https://twitter.com/harry_styles?lang=en"
+
+page = requests.get(harry_url)
+print(page)
+
+soup = BeautifulSoup(page.text, "html.parser")
+print(soup.prettify())
+
+tweets = soup.find_all('span', class_="css-901oao")
+
+print(tweets)
+
+'''
+# print("{} {}!".format("Hello", "World"))
 
 
 # Weather Scraping (15pts)
@@ -23,5 +52,52 @@
 # Wednesday, April 4 will be Partly Cloudy/Windy with a High of 37 degrees and a low of 25, humidity at 52%.  There is 0% chance of rain with winds out of the WNW at 22 mph.
 # if the sentence is a little different than shown, that will work; do what you can.  Don't forget about our friend string.format()
 
+url = "https://weather.com/weather/tenday/l/69bedc6a5b6e977993fb3e5344e3c06d8bc36a1fb6754c3ddfb5310a3c6d6c87"
 
+page = requests.get(url)
+print(page)
+
+soup = BeautifulSoup(page.text, "html.parser")
+print(soup.prettify())
+
+print("\n" * 5)
+
+day_tags = soup.find_all("span", class_="date-time")  # is it okay
+day_list = [x.text for x in day_tags]
+print(day_list[:10])
+
+date_tags = soup.find_all("span", class_="day-detail clearfix")
+date_list = [x.text for x in date_tags]
+print(date_list[:10])
+
+description_tags = soup.find_all("td", class_="description")
+description_list = [x.text for x in description_tags]
+print(description_list[:10])
+
+high_low_tags = soup.find_all("td", class_="temp")
+high_low_list = [x.text.replace("°", "°/", 1) for x in high_low_tags]
+print(high_low_list[:10])
+
+rain_tags = soup.find_all("td", class_="precip")
+rain_list = [x.text for x in rain_tags]
+print(rain_list[:10])
+
+wind_tags = soup.find_all("td", class_="wind")
+wind_list = [x.text for x in wind_tags]
+print(wind_list[:10])
+
+humidity_tags = soup.find_all("td", class_="humidity")
+humidity_list = [x.text for x in humidity_tags]
+print(humidity_list[:10])
+
+print("\n" * 5)
+
+print("San Fransisco, CA, 10 Day Weather")
+print("_______________________________________")
+for i in range(len(day_list)):  # when to use .format?
+    print(day_list[i] + ",", date_list[i], "will be", description_list[i],
+          "with a high and low of", high_low_list[i], ", humidity at", humidity_list[i] + ". There is a", rain_list[i], "chance of rain with winds of", wind_list[i]+".")
+    print()
+
+    # fix high and low temp & winds
 
